da24c54ce4c42571820e7770cd9069d9b9e76fcd
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-01-13 15:17:51 +0300

    13.01

---
+++ BlogAPIDemo/Controllers/DataAccessLayer/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogAPIDemo.Controllers.DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-5UUIH70;database=CoreBlogApiDb; integrated security=true;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
