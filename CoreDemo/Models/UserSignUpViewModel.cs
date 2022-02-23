ef5e503cfc2adac1ef3ca9cc1d549452022d32b3
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-02-23 16:15:10 +0300

    s

---
+++ CoreDemo/Models/UserSignUpViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Models
+{
+    public class UserSignUpViewModel
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Boş bırakma")]
+        public string NameSurname { get; set; }
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Boş bırakma")]
+        public string Password { get; set; }
+        [Display(Name = "Şifre Tekrarla")]
+        [Compare("Password",ErrorMessage = "Boş bırakma")]
+        public string ConfirmPassword { get; set; }
+        [Display(Name = "Mail")]
+        [Required(ErrorMessage = "Boş bırakma")]
+        public string Mail { get; set; }
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Boş bırakma")]
+        public string UserName { get; set; }
+    }
+}
---
+++ DataAccessLayer/Migrations/20220223121955_mıjh.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class mıjh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NameSurname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "imageUrl",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameSurname",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "imageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
